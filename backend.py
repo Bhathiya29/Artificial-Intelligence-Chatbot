584eafb9db6408d3a8b94106310bb39f421f3b93
bhathiya <bhathiyabandara87@gmail.com>
2023-07-08 09:15:02 +0530

    Final Commit - completed the backend.py and connecting it with main.py

---
+++ backend.py
@@ -0,0 +1,24 @@
+# THE BACK-END OF THE CHATBOT
+import openai
+
+
+class Chatbot:
+    def __init__(self):
+        openai.api_key = "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"
+
+    def get_response(self, user_input):
+        response = openai.Completion.create(
+            engine='text-davinci-003',  # Algorithm
+            prompt=user_input,  # The users input
+            max_tokens=3000,  # Max no of words
+            temperature=0.5  # Accuracy and diversity of answers
+
+        ).choices[0].text
+
+        return response
+
+
+if __name__ == '__main__':
+    chatbot =Chatbot()
+    response = chatbot.get_response('Write a joke')
+    print(response)
--- main.py
+++ main.py
@@ -1,34 +1,48 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
 import sys
+from backend import Chatbot
+import threading
 
 
+# FRONT-END GUI
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
         # Adding the chat area widget
         self.chat_area = QTextEdit(self)
-        self.chat_area.geometry(10, 10, 480, 320)  # Coordinates of the area
+        self.chat_area.setGeometry(10, 10, 480, 320)  # Coordinates of the area
         self.chat_area.setReadOnly(True)
 
         # Adding the input field widget
         self.input_field = QLineEdit(self)
-        self.input_field.geometry(10, 340, 480, 40)  # Coordinates of the area
+        self.input_field.setGeometry(10, 340, 480, 40)  # Coordinates of the area
+        self.input_field.returnPressed.connect(self.send_message())  # On clicking enter it works same as send button
 
         # Adding the button
         self.button = QPushButton('Send', self)
-        self.button.geometry(500, 340, 100, 40)  # Coordinates of the button
-
+        self.button.setGeometry(500, 340, 100, 40)  # Coordinates of the button
+        self.button.clicked.connect(self.send_message)
 
         self.show()
 
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>Me : {user_input},</p>")
+        self.input_field.clear()
 
-class Chatbot:
-    pass
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333'; background-color:#E9E9E9>Bot {response}</p>")
 
 
-app = QApplication()
+app = QApplication(sys.argv)
 main_window = ChatbotWindow()
 sys.exit(app.exec())
